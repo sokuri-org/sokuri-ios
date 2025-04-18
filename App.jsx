1efb50c63dcabdf14cd1cba1d5b05643003ad0c4
sekyeong <sekoonglee@gmail.com>
2025-04-19 02:02:26 +0900

    design: 메인 레이아웃 개선

--- App.jsx
+++ App.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
-import ARBridge from './src/native/ARBridge';
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
-  useEffect(() => {
-    ARBridge.getDeviceInfo((err, result) => {
-    if (result) {
-      console.log('✅ ARBridge 응답:', result);
-    }
-
-    throw err.message;
-    });
-  }, []);
   return (
-    <View>
-      <Text>소쿠리 앱 실행!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <HomeScreen />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
