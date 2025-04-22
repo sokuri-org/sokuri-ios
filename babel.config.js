34157723d947374f04f0eeae9df7fc8ea0f1cfb3
sekyeong <sekoonglee@gmail.com>
2025-04-22 14:12:04 +0900

    chore: 프로젝트 빌드 및 환경 설정 파일 업데이트

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          "@": "./src",
+        },
+      },
+    ],
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
